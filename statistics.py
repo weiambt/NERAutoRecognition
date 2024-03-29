d0c9506e3fa969806fc74e989d3fe54daca91836
weiambt <itzwei@163.com>
2024-03-29 19:37:20 +0800

    [refactor](优化统计实体)统计所有实体的数量

--- statistics.py
+++ statistics.py
@@ -1,6 +1,7 @@
 from collections import Counter
 
-def count_entities(input_file, entities):
+
+def count_entities(input_file):
     mp = Counter()
     with open(input_file, 'r', encoding='utf-8') as f:
         cnt = 0
@@ -14,9 +15,10 @@ def count_entities(input_file, entities):
             tmp = char_label.split('-')
             print(tmp, cnt)
             pos, entity = tmp[0], tmp[1]
-            if entity in entities and pos == 'B':
+            if pos == 'B':
                 mp[entity] += 1
-    print(mp)
+    print('The number of each entity in the file named 【%s】' % input_file, end='--->')
+    print(list(mp.items()))
 
 
 def count_sentences(input_file):
@@ -26,7 +28,7 @@ def count_sentences(input_file):
             if line[0] in ['。']:
                 cnt += 1
     # print(str)
-    print('the length of sentences is %d' % cnt)
+    print('The length of sentences in the file named 【%s】 is %d' % (input_file, cnt))
 
 
 if __name__ == '__main__':
@@ -34,7 +36,7 @@ if __name__ == '__main__':
     # count_entities('爆炸_2024-3-23_output.bio',['TYPE','PROPERTYLOSS','CASUALTY'])
 
     # Counter({'TYPE': 150, 'CASUALTY': 36, 'PROPERTYLOSS': 15})
-    count_entities('爆炸_2024-3-23_train.bio', ['TYPE', 'PROPERTYLOSS', 'CASUALTY'])
+    count_entities('爆炸_2024-3-23_train.bio')
     count_sentences('爆炸_2024-3-23_train.bio')
     # 训练集占比
     # Type:0.77,CASUALTY:0.83,PROPERTYLOSS:0.71
